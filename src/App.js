c1adbc949d6abbc1d5e82883a8fc6b686b296e2e
Lucas Gomez <exequiel_river@live.com>
2022-06-07 22:39:47 -0300

    desafio2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './random.png';
 import './App.css';
-
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>HOLA MUNDO !</h1>
-      </header>
+      <NavBar/>
     </div>
   );
 }
-
 export default App;
