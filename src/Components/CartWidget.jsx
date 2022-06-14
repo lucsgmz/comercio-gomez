12473bf31e71bdc2c0e708127dab23a1de11d854
Lucas Gomez <exequiel_river@live.com>
2022-06-13 23:28:25 -0300

    desafio 3 correcion

--- src/Components/CartWidget.jsx
+++ src/Components/CartWidget.jsx
@@ -7,7 +7,7 @@ function CartWidget() {
     <div className="flex flex-col lg:flex-row text-center">
       <a href="#">
         <img src={carrito} alt="ALGO" />
-        
+        <p className=" text-violet-700 font-mono">3</p>
       </a>
     </div>
   );
